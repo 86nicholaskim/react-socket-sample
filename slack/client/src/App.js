ca9b488228d94dd932da03627e35c2e8ccb3c023
seongbeom kim <86nichola@gmail.com>
2024-06-29 09:09:36 +0900

    slack init client files

--- slack/client/src/App.js
+++ slack/client/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { IndexContainer, MainContainer } from './containers';
+import { StoreProvider } from './context';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <StoreProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<IndexContainer />} />
+          <Route path="/main" element={<MainContainer />} />
+        </Routes>
+      </Router>
+    </StoreProvider>
+  );
 }
 
 export default App;
