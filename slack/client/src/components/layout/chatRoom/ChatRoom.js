3655244d586ad4377f72b7deb414f2ddebd724c2
seongbeom kim <86nichola@gmail.com>
2024-07-05 00:01:54 +0900

    client work #2

--- slack/client/src/components/layout/chatRoom/ChatRoom.js
+++ slack/client/src/components/layout/chatRoom/ChatRoom.js
@@ -21,47 +21,172 @@ const ChatRoom = () => {
   const [text, setText] = useState('');
   const [groupUser, setGroupUser] = useState('');
   const [msgList, setMsgList] = useState([]);
-  const [groupChatUser, setGroupChatUsers] = useState([]);
+  const [groupChatUsers, setGroupChatUsers] = useState([]);
 
-  useEffect(function privateMsg() {
-    socketPrivate.on('private-msg', setPrivateMsgListHandler);
-    return () => {
-      socketPrivate.off('private-msg', setPrivateMsgListHandler);
-    };
+  useEffect(
+    function privateMsg() {
+      socketPrivate.on('private-msg', setPrivateMsgListHandler);
+      return () => {
+        socketPrivate.off('private-msg', setPrivateMsgListHandler);
+      };
 
-    function setPrivateMsgListHandler(data) {
-      const { msg, fromUserId, toUserId, time } = data;
-      if (
-        currentChat.roomNumber === `${fromUserId}-${toUserId}` ||
-        currentChat.roomNumber === `${toUserId}-${fromUserId}`
-      ) {
-        setMsgList((prev) => [
-          ...prev,
-          {
-            msg,
-            userId: fromUserId,
-            time,
-          },
-        ]);
+      function setPrivateMsgListHandler(data) {
+        const { msg, fromUserId, toUserId, time } = data;
+        if (
+          currentChat.roomNumber === `${fromUserId}-${toUserId}` ||
+          currentChat.roomNumber === `${toUserId}-${fromUserId}`
+        ) {
+          setMsgList((prev) => [
+            ...prev,
+            {
+              msg,
+              userId: fromUserId,
+              time,
+            },
+          ]);
+        }
       }
+    },
+    [currentChat.roomNumber]
+  );
+
+  useEffect(
+    function groupMsg() {
+      socketGroup.on('group-msg', setGroupMsgListHandler);
+      return () => {
+        socketGroup.off('group-msg', setGroupMsgListHandler);
+      };
+      function setGroupMsgListHandler(data) {
+        const { msg, toUserSocketId, fromUserId, time } = data;
+        if (currentChat.roomNumber === toUserSocketId) {
+          setMsgList((prev) => [...prev, { msg, userId: fromUserId, time }]);
+        }
+      }
+    },
+    [currentChat.roomNumber]
+  );
+
+  useEffect(
+    function msgListInit() {
+      return () => {
+        setMsgList([]);
+      };
+    },
+    [currentChat.roomNumber]
+  );
+
+  useEffect(function privateMsgInit() {
+    socketPrivate.on('private-msg-init', setMsgListInit);
+    return () => {
+      socketPrivate.off('private-msg-init', setMsgListInit);
+    };
+    function setMsgListInit(data) {
+      setMsgList(
+        data.msg.map((m) => ({
+          msg: m.msg,
+          userId: m.fromUserId,
+          time: m.time,
+        }))
+      );
     }
   }, []);
-  useEffect(function groupMsg() {}, []);
-  useEffect(function privateMsgInit() {}, []);
-  useEffect(function groupMsgInit() {}, []);
-  useEffect(function msgListInit() {}, []);
 
-  const onPrivateMsgSendHandler = () => {};
-  const onGroupSendHandler = () => {};
-  const onChangeGroupTextHandler = () => {};
-  const groupChatUserCloseClick = () => {};
-  const onJoinClick = () => {};
-  const onGroupMsgSendHandler = () => {};
+  useEffect(function groupMsgInit() {
+    socketGroup.on('group-msg-init', setGroupMsgListInit);
+    return () => {
+      socketGroup.off('group-msg-init', setGroupMsgListInit);
+    };
+    function setGroupMsgListInit(data) {
+      setMsgList(
+        data.msg.map((m) => ({
+          msg: m.msg,
+          userId: m.fromUserId,
+          time: m.time,
+        }))
+      );
+    }
+  }, []);
+
+  const onPrivateMsgSendHandler = () => {
+    const msg = reactQuillRef.current.unprivileageEditor.getText();
+    const currentTime = dayjs().format('HH:mm a');
+    setMsgList((prev) => [
+      ...prev,
+      { msg, userId: loginInfo.userId, time: currentTime },
+    ]);
+    socketPrivate.emit('privateMsg', {
+      msg,
+      toUserId: currentChat.targetId[0],
+      toUserSocketId: currentChat.targetSocketId,
+      fromUserId: loginInfo.userId,
+      time: currentTime,
+    });
+    setText('');
+  };
+
+  const onGroupSendHandler = (e) => {
+    e.preventDefault();
+    if (!userList.filter((v) => v.userId === groupUser).length > 0) {
+      alert('The user does not exist.');
+      setGroupUser('');
+      return;
+    }
+    if (groupUser === loginInfo.userId) {
+      alert('Please, Choose someone else.');
+      setGroupUser('');
+      return;
+    }
+    setGroupChatUsers([...groupChatUsers, groupUser]);
+    setGroupUser('');
+  };
+
+  const onChangeGroupTextHandler = (e) => {
+    setGroupUser(e.target.value);
+  };
+
+  const groupChatUserCloseClick = (e) => {
+    const { id } = e.target.dataset;
+    setGroupChatUsers(groupChatUsers.filter((v) => v !== id));
+  };
+
+  const onJoinClick = () => {
+    if (groupChatUsers.length <= 0) return;
+    const socketId = [...groupChatUsers, loginInfo.userId].join(',');
+    const user = { socketId, status: true, userId: socketId, type: 'group' };
+    socketGroup.emit('reqGroupJoinRoom', user);
+    setGroupChatUsers([]);
+  };
+
+  const onGroupMsgSendHandler = () => {
+    const msg = reactQuillRef.current.unprivileageEditor.getText();
+    const currentTime = dayjs().format('HH:mm a');
+    setMsgList((prev) => [
+      ...prev,
+      { msg, userId: loginInfo.userId, time: currentTime },
+    ]);
+    socketGroup.emit('groupMsg', {
+      toUserId: currentChat.targetSocketId,
+      toUserSocketId: currentChat.targetSocketId,
+      fromUserId: loginInfo.userId,
+      msg,
+      time: currentTime,
+    });
+    setText('');
+  };
 
   return (
     <article css={chatRoomWrapCss}>
       <div css={subTitleCss}>
-        {gropuChat.textBarStatus && <GroupTextInput />}
+        {gropuChat.textBarStatus && (
+          <GroupTextInput
+            groupText={groupUser}
+            onChangeGroupTextHandler={onChangeGroupTextHandler}
+            groupChatUserList={groupChatUsers}
+            onGroupSendHandler={onGroupSendHandler}
+            groupChatUserCloseClick={groupChatUserCloseClick}
+            onJoinClick={onJoinClick}
+          />
+        )}
         {!gropuChat.textBarStatus &&
           currentChat.targetId.map((v) => {
             <span className="user">{v}</span>;
@@ -70,11 +195,12 @@ const ChatRoom = () => {
       {currentChat.roomNumber && (
         <ul css={chatBoxCss}>
           {msgList.map((v, i) => (
-            <li css={chatCss}>
+            <li css={chatCss} key={`${i}-chat`}>
               <div className="userBox">
                 <span className="user">{v.userId}</span>
                 <span className="date">{v.time}</span>
               </div>
+              <div className="textBox">{v.msg}</div>
             </li>
           ))}
         </ul>
